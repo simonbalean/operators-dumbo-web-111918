8f59d81244d6724f5293178b4a3d068c06f6ae9c
Simon Balean <simonbalean@gmail.com>
2018-11-13 23:26:13 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    return TRUE
+  else return FALSE
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+#	speed < 40 || speed > 60 ? return TRUE : return FALSE
+  return TRUE if speed < 40 || > 60
 end
 	
 
